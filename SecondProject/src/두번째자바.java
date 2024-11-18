7b1cfc87f69ac40ffbe0607d47e2c721becfe057
USER <USER@DESKTOP-JM4DKDH>
2024-11-18 14:41:24 +0900

    2222

--- SecondProject/src/두번째자바.java
+++ SecondProject/src/두번째자바.java
@@ -1,4 +1,10 @@
-
+\*
+Git 연동
+  1. 파일 오른쪽 버튼 - Team - share
+  2. file 저장 위치는 깃에 저장한 파일명
+  3. Unstaged Changes에 있는 파일을 ++버튼으로 아래로 옮기기
+  4. Commit Massage 쓰고 Commit and push 버튼 누르기
+  5. Git 아이디와 토큰
 public class 두번째자바 {
 
 	public static void main(String[] args) {
