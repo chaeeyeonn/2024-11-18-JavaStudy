4c094930af078a5e29a02f6333c6dbb9620d6086
USER <USER@DESKTOP-JM4DKDH>
2024-11-18 15:19:13 +0900

    3333

---
+++ ThreeProject/src/MainClass.java
@@ -0,0 +1,14 @@
+/*
+cmd에 넣을 때
+1. javac (파일명).java - class 파일 만드는 과정
+(컴파일: 파일을 기계언어로 변경/컴퓨터가 인식하는 언어 - Java, C#, C, C++)
+2. java (파일명) - java 파일 출력(인터프리터 - HTML, Java ...)
+*/
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Java!!");
+	}
+
+}
