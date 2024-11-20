20ac18b7d21b4b4fc63714601b8037ed9a398004
USER <USER@DESKTOP-JM4DKDH>
2024-11-20 17:46:02 +0900

    241120

---
+++ Chapter02_연산자/src/연산자정리_기타.java
@@ -0,0 +1,20 @@
+/*
+ *   특수문자
+ *   *** \n : new line , 다음 줄에 출력
+ *   \t : tab, 일정간격을 벌린다
+ *   \" : "를 출력
+ *   *** \\: \를 출력, 경로명 쓸 떄
+ */
+public class 연산자정리_기타 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("c:\\javaDev\\javaStudy");// \가 하나 출력
+		System.out.println("Hello \n");// 다음 줄로 출력
+		System.out.println("Java");
+		System.out.println("Hello\tHello\tHello");// 간격 띄어줌
+		System.out.println("\"홍길동\"");// "출력
+
+	}
+
+}
