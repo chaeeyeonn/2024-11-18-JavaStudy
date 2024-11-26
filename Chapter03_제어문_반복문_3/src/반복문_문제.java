0543bbee49d0a92332c1b95139a3d010da02a73e
USER <USER@DESKTOP-JM4DKDH>
2024-11-26 17:50:08 +0900

    241126

--- Chapter03_제어문_반복문_3/src/반복문_문제.java
+++ Chapter03_제어문_반복문_3/src/반복문_문제.java
@@ -182,20 +182,45 @@ public class 반복문_문제 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=1;i<=5;i++)
-		{   char c='A';
-			for(char j=1;j<=5;j++)
-			{
-			  
-				System.out.print(c++);
-			
-				
-			}
-			
-			System.out.println();
-		
-		}
+//		for(int i=1;i<=5;i++)
+//		{   char c='A';
+//			for(char j=1;j<=5;j++)
+//			{
+//			  
+//				System.out.print(c++);
+//			
+//				
+//			}
+//			
+//			System.out.println();
+//		
+//		}
+//	}
+//}
+
+// ★☆☆☆★
+// ☆☆☆☆☆
+// ☆☆☆☆☆
+// ☆☆☆☆☆
+// ★☆☆☆★
+// 
+
+ for(int i=1;i<=5;i++)
+ {
+	 for(int j=1;j<=5;j++)
+	 {
+		 if((i==1 && j==1)||(i==1 && j==5)
+		   || (i==5 && j==1)||(i==5 && j==5))			 
+			 System.out.print("★");
+		 else
+		 System.out.print("☆");
+		 
+	 }
+	 System.out.println();
+ }
 	}
 }
+ 
 
-//
+   
+   
