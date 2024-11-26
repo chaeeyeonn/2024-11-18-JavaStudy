942e948f1972a9732f87ed848b20fb7e828e868f
USER <USER@DESKTOP-JM4DKDH>
2024-11-26 17:35:46 +0900

    241126

--- Chapter03_제어문_반복문_3/src/반복문_문제.java
+++ Chapter03_제어문_반복문_3/src/반복문_문제.java
@@ -181,19 +181,19 @@ public class 반복문_문제 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char c='A';
+		
 		for(int i=1;i<=5;i++)
-		{
+		{   char c='A';
 			for(char j=1;j<=5;j++)
 			{
 			  
-				System.out.print(c);
+				System.out.print(c++);
 			
 				
 			}
 			
 			System.out.println();
-			c++;
+		
 		}
 	}
 }
