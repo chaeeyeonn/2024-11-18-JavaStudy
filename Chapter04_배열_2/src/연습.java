e2ae41d497169bcfcd5a5b14b04a083cec68be4c
USER <USER@DESKTOP-JM4DKDH>
2024-12-02 17:51:55 +0900

    241202

--- Chapter04_배열_2/src/연습.java
+++ Chapter04_배열_2/src/연습.java
@@ -39,19 +39,19 @@ public class 연습 {
 //      System.out.println();
 //      System.out.printf("평균:%.2f",sum/10.0);
         
-// 6. int[] num = { 94, 85, 95, 88, 90 };
+// 6. int[] num = { 94, 85, 95, 88, 90 };//명시적 초기화
 //    다음 배열에서 최대값,최소값을 출력하는 프로그램을 작성하시오
         
 //      int[] num = { 94, 85, 95, 88, 90 };
-//      int max=num[0];
+//      int max=num[0];//최대, 최소값을 무조건 0,100으로 놓을 수 없기에 인덱스 첫 번째 값으로 둠
 //      int min=num[0];
 //      
-//      for(int i=0;i<num.length;i++)
+//      for(int i:num)
 //      {
-//    	  if(max<num[i])
-//    		  max=num[i];
-//    	  if(min>num[i])
-//    		  min=num[i];
+//    	  if(max<i)
+//    		  max=i;
+//    	  if(min>i)
+//    		  min=i;
 //      }
 //      System.out.printf("최대값: %d 최소값: %d",max,min);
         
@@ -60,16 +60,24 @@ public class 연습 {
 //      System.out.println(arr[1]);
 
 // 9.  arr 배열 중에서 값이 60인 곳의 인덱스를 출력해보자. 
-      int[] arr={10,20,30,50,3,60,-3};
-      for(int i=0;i<arr.length;i++)
-      {
-    	  if(arr[i]==60)
-      System.out.println(i);
-      }
-
-
-
-        
+//      int[] arr={10,20,30,50,3,60,-3};
+//      int index=0;
+//      for(int i=0;i<arr.length;i++)
+//      {
+//    	  if(arr[i]==60)
+//    	  {
+//    		  index=i;
+//    		  break;
+//    	  }
+//    	 
+//      }
+//      System.out.println("인덱스:"+index);
+        /*
+         *   지역변수 = 블록변수 => 메모리에서 사라진다
+         *   {} => 메모리 자체에서 관리
+         *         --------Stack: 지역변수/매개변수
+         *   변수의 사용 범위: scope ({}안에서만 사용 가능하다)
+         */
 	}
 
 }
