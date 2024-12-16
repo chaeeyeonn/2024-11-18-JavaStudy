1bb127f0590a43d26cf7dfa6f8554e69994235a8
USER <USER@DESKTOP-JM4DKDH>
2024-12-16 17:13:35 +0900

    241216

---
+++ Chapter07_인터페이스/src/com/sist/main/인터페이스_1.java
@@ -0,0 +1,27 @@
+package com.sist.main;
+/*
+ *   인터페이스
+ *   -------
+ *   1) 추상클래스의 일종(클래스와 동일 취급)
+ *   2) 추상클래스: 단일 상속
+ *      인터페이스: 다중 상속
+ *   3) 인터페이스
+ *        변수(X) => 상수
+ *        메소드(구현 안함)) => 구현 안 된 메소드만 가지고 있따
+ *   4) 결합성
+ *   
+ *   
+ */
+interface A 
+{
+  
+   
+}
+public class 인터페이스_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter07_인터페이스응용_2/src/com/sist/main/클래스종류정리.java
@@ -0,0 +1,12 @@
+package com.sist.main;
+/*
+ *   
+ */
+public class 클래스종류정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
