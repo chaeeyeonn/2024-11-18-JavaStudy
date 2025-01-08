e91f1ff980db66e3ef4aa68fd93a7137d988758b
김채연 <rlaecodusx@gmail.com>
2025-01-08 12:47:20 +0900

    250108

---
+++ 오라클데이터베이스_3/src/com/sist/dao/EmpDAO.java
@@ -0,0 +1,132 @@
+package com.sist.dao;
+// 데이터베이스 연결 클래스
+import java.util.*;
+import java.sql.*;
+import com.sist.vo.*;
+/*
+ *   오라클 => SQL
+ *   자바에서도 실행하는지 => 자바에서 오라클 SQL 문장 정리
+ */
+public class EmpDAO {
+	// 오라클 연결
+	private Connection conn;
+	// 오라클 SQL전송=결과값
+	private PreparedStatement ps;
+	// DAO => 사용자당 한 개만 생성 => 메모리 최소화(Connection 조절)
+	private static EmpDAO dao;
+	// 고정 데이터: URL(오라클 주소)
+	private final String URL="jdbc:oracle:thin:@localhost:1521:XE";
+	// XE: 데이터베이스명 => 모든 테이블이 들어가 있다
+	// => 폴더(파일 여러 개)
+	// 1. 드라이버 등록 => 한 번만 사용
+	public EmpDAO()
+	{
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// 클래스명으로 메모리 할당
+			/*
+			 *   1. 생성자
+			 *   2. 멤버변수
+			 *   3. 메소드 => 매개변수도 처리
+			 *   ==> 모두 제어 가능
+			 *   => 리플렉션 => 스프링 / 메소드 자동호출 가능
+			 */
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	// 싱글턴 만들기 => 메모리 공간 감소 / 재사용 가능
+	// newInstance() / getInstance() => 싱글턴
+	// Calendar.getInstance()
+	// 파싱: XML / HTML / JSON
+	public static EmpDAO newInstance()
+	{
+		if(dao==null)
+			dao=new EmpDAO();
+		return dao;
+	}
+	// 오라클 연결
+	public void getConnection()
+	{
+		try
+		{
+			// sqlplus과 같음
+			conn=DriverManager.getConnection(URL,"hr","happy");
+		}catch(Exception ex) {}
+	}
+	// 오라클 해제
+	public void disConnection()
+	{
+		try
+		{
+			if(ps!=null) ps.close();
+			if(conn!=null) conn.close();
+		}catch(Exception ex) {}
+	}
+	//-----------------------------------------------공통사항
+	/*
+	 *   DAO => 테이블 한 개에 대한 제어
+	 *   예) BoardDAO / ReplyDAO
+	 *   Service => 관련 테이블 모두에 대한 제어
+	 *   예) BoardService: Board / Reply
+	 */
+	// 1. 기능
+	/*
+	 *  사원(사번, 이름, 직위, 입사일, 급여)
+	 *  부서(부서명, 근무지)
+	 *  등급(등급)
+	 *  ==> 테이블 3개에서 출력에 필요한 데이터 추출 => JOIN 사용
+	 */
+	public List<EmpVO> empJoinAllData()
+	{
+		List<EmpVO> list=new ArrayList<EmpVO>();
+		try
+		{
+			//1. 연결
+			getConnection();
+			//2. 오라클로 SQL문장 전송
+			String sql="SELECT DISTINCT empno,ename,job,hiredate,TO_CHAR(sal,'$999,999'),dname,loc,grade FROM emp,dept,salgrade "
+					   +"WHERE emp.deptno=dept.deptno AND sal BETWEEN losal AND hisal";
+			//2-1. 전송
+			ps=conn.prepareStatement(sql);
+			//3. SQL 실행 후 결과값을 메모리에 저장
+			ResultSet rs=ps.executeQuery();
+			//4. 커서 위치 변경 => 첫 번째 출력 위치로
+			/*
+			 * 현재 값 => 순서대로 1번부터 시작
+			 * empno,ename,job,hiredate,sal,dname,loc,grade
+			 * int  String String Date  int  String String int
+			 * => getInt() / getString() / getDate()
+			 */
+			while(rs.next())
+			{
+				EmpVO vo=new EmpVO();// Record 단위 => 한 줄씩 읽는다
+				vo.setEmpno(rs.getInt(1));
+				vo.setEname(rs.getString(2));
+				vo.setJob(rs.getString(3));
+				vo.setHiredate(rs.getDate(4));
+				//vo.setSal(rs.getInt(5));
+				vo.setStrSal(rs.getString(5));
+				//dvo
+				vo.getDvo().setDname(rs.getString(6));
+				vo.getDvo().setLoc(rs.getString(7));
+				//svo
+				vo.getSvo().setGrade(rs.getInt(8));
+				
+				list.add(vo);
+			}
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			disConnection();
+		}
+		return list;
+		
+	}
+
+}
---
+++ 오라클데이터베이스_3/src/com/sist/vo/EmpVO.java
@@ -0,0 +1,104 @@
+package com.sist.vo;
+// 사원 정보 => 사원 한 명에 대한 정보 저장
+// => 모아서 한 번에 윈도우나 브라우저에 전송할 목적
+/*
+ EMPNO    NOT NULL NUMBER(4) = int
+ ENAME    VARCHAR2(10) = String
+ JOB      VARCHAR2(9) = String
+ MGR      NUMBER(4) = int
+ HIREDATE DATE = java.util.Date
+ SAL      NUMBER(7,2) = int
+ COMM     NUMBER(7,2) = int
+ DEPTNO   NUMBER(2) = int
+ -----------------------------=> 오류: 내부변환이 안 된다
+ => 오라클
+    문자 = CHAR / VARCHAR2 / CLOB => String
+    숫자 = NUMBER => double / int
+    날짜 = DATE => java.util.Date
+ */
+import java.util.*;
+// => 테이블 연동: 다른 테이블의 컬럼 추가하면 안 됨
+// => 테이블 여러 개 컬럼을 섞어서 사용하려면 => 한 개의 테이블로 변경 => View(가상테이블)
+public class EmpVO {
+	private int empno,mgt,sal,comm,deptno;
+	private String ename,job;
+	private Date hiredate;
+	private String strSal; // 문자열
+	// TO_CHAR => 문자열 변환 => String
+	// => 문자열을 받을 수 있게 String 변수 추가
+	/*
+	 *  1. 정수(천 단위,)
+	 *  2. 날짜 변환
+	 */
+	
+	public String getStrSal() {
+		return strSal;
+	}
+	public void setStrSal(String strSal) {
+		this.strSal = strSal;
+	}
+	//조인 => 포함 클래스 => List<EmpVO>를 위함
+	private DeptVO dvo=new DeptVO();
+	private SalGradeVO svo=new SalGradeVO();
+	
+	public DeptVO getDvo() {
+		return dvo;
+	}
+	public void setDvo(DeptVO dvo) {
+		this.dvo = dvo;
+	}
+	public SalGradeVO getSvo() {
+		return svo;
+	}
+	public void setSvo(SalGradeVO svo) {
+		this.svo = svo;
+	}
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public int getMgt() {
+		return mgt;
+	}
+	public void setMgt(int mgt) {
+		this.mgt = mgt;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public int getComm() {
+		return comm;
+	}
+	public void setComm(int comm) {
+		this.comm = comm;
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public Date getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(Date hiredate) {
+		this.hiredate = hiredate;
+	}
+}
